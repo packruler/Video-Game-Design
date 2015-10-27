510212e0231b6575edb12025fa04c289a9537afd
Ethan <packruler@gmail.com>
2015-10-27 12:33:11 -0400

    A* pathfinding

--- src/edu/utc/leisinger3520/game/gridStuff/GridCell.java
+++ src/edu/utc/leisinger3520/game/gridStuff/GridCell.java
@@ -1,6 +1,7 @@
 package edu.utc.leisinger3520.game.gridStuff;
 
 import edu.utc.leisinger3520.game.display.Color;
+import edu.utc.leisinger3520.game.logging.Log;
 import edu.utc.leisinger3520.game.objects.Entity;
 import org.lwjgl.opengl.GL11;
 
@@ -10,18 +11,118 @@ import org.lwjgl.opengl.GL11;
 public class GridCell extends Entity {
     private Color outline = new Color(0, 0, 0);
     private Color fill = new Color();
+    private int status;
+    public static final int EMPTY = 0;
+    public static final int OBSTACLE = 1;
+    public static final int PLAYER = 2;
+    public static final int TARGET = 3;
+    private int distanceFromPlayer = 0;
+    private boolean found = false;
+    private int row, col, fromRow, fromCol;
+    private final GridGame game;
+    private static final Color onPath = new Color(.5, .5, 1);
 
-    public GridCell(int x, int y, int w, int h) {
-        hitbox.setBounds(x, y, w, h);
-        active = Math.random() >= .5;
+    public GridCell(int col, int row, GridGame game, int status) {
+        hitbox.setBounds(col * game.cellWidth, row * game.cellHeight, game.cellWidth, game.cellHeight);
+        this.col = col;
+        this.row = row;
+        this.game = game;
+        setStatus(status);
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+        switch (status) {
+            case EMPTY:
+                fill = new Color(1, 1, 1);
+                break;
+            case OBSTACLE:
+                fill = new Color(0, 0, 0);
+                break;
+            case PLAYER:
+                fill = new Color(0, 1, 0);
+                break;
+            case TARGET:
+                fill = new Color(1, 0, 0);
+                break;
+        }
+    }
+
+    public void findTarget() {
+        if (row - 1 >= 0)
+            checkNext(col, row - 1);
+
+        if (col - 1 >= 0)
+            checkNext(col - 1, row);
+
+        if (row + 1 < game.rowCount)
+            checkNext(col, row + 1);
+
+        if (col + 1 < game.colCount)
+            checkNext(col + 1, row);
+    }
+
+    public void checkNeighbors(final int fromCol, final int fromRow, int distance) {
+        found = true;
+        distance++;
+//        Log.i("Row: " + row + " Col: " + col + " Dist: " + distanceFromPlayer);
+//        Log.i("Current distance: " + distanceFromPlayer + "New distance: " + distance);
+        if (status == TARGET) {
+            Log.i("TARGET FOUND From: " + fromCol + "," + fromRow + " Dist: " + distance);
+            if (distanceFromPlayer == 0 || distanceFromPlayer > distance) {
+                Log.i("Update path");
+                this.fromCol = fromCol;
+                this.fromRow = fromRow;
+                distanceFromPlayer = distance;
+                game.path.clear();
+                reversePath();
+            }
+        } else if (distanceFromPlayer == 0 || distanceFromPlayer > distance) {
+            distanceFromPlayer = distance;
+            this.fromCol = fromCol;
+            this.fromRow = fromRow;
+            if (row - 1 >= 0)
+                checkNext(col, row - 1);
+
+            if (col - 1 >= 0)
+                checkNext(col - 1, row);
+
+            if (row + 1 < game.rowCount)
+                checkNext(col, row + 1);
+
+            if (col + 1 < game.colCount)
+                checkNext(col + 1, row);
+        }
+    }
+
+    private void checkNext(int x, int y) {
+        GridCell nextCell = game.grid[x][y];
+        if (!(nextCell.isFound() && nextCell.distanceFromPlayer < distanceFromPlayer) && nextCell.getStatus() != OBSTACLE) {
+            nextCell.checkNeighbors(col, row, distanceFromPlayer);
+        }
+    }
+
+    private void reversePath() {
+        if (status == EMPTY)
+            game.path.add(this);
+
+        if (status != PLAYER)
+            game.grid[fromCol][fromRow].reversePath();
+    }
+
+    public boolean isFound() {
+        return found;
+    }
+
+    public int getStatus() {
+        return status;
     }
 
     @Override
     public void draw() {
         drawOutline();
 
-        if (active)
-            drawFill();
+        drawFill();
     }
 
     protected void drawOutline() {
@@ -40,7 +141,11 @@ public class GridCell extends Entity {
     protected void drawFill() {
         GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
         GL11.glBegin(GL11.GL_QUADS);
-        fill.use();
+
+        if (game.path.contains(this))
+            onPath.use();
+        else
+            fill.use();
 
         GL11.glVertex2i(hitbox.x, hitbox.y);
         GL11.glVertex2i(hitbox.x + hitbox.width, hitbox.y);
--- src/edu/utc/leisinger3520/game/gridStuff/GridGame.java
+++ src/edu/utc/leisinger3520/game/gridStuff/GridGame.java
@@ -1,32 +1,57 @@
 package edu.utc.leisinger3520.game.gridStuff;
 
-import edu.utc.leisinger3520.game.objects.Entity;
 import edu.utc.leisinger3520.game.scenes.Scene;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
+
+import java.util.LinkedList;
 
 /**
  * Created by Ethan Leisinger on 10/21/2015.
  */
 public class GridGame extends Scene {
-    private int xSize = 10;
-    private int ySize = 10;
-    private Entity[][] grid = new Entity[xSize][ySize];
-    private int cellHeight, cellWidth;
+    public int colCount = 10;
+    public int rowCount = 10;
+    public GridCell[][] grid = new GridCell[colCount][rowCount];
+    public final int cellHeight, cellWidth;
+    private GridCell player = null;
+    private GridCell target = null;
+    public LinkedList<GridCell> path = new LinkedList<>();
 
     public GridGame() {
         int yLoc = 0;
         int xLoc = 0;
-        cellHeight = Display.getHeight() / ySize;
-        cellWidth = Display.getWidth() / xSize;
-        for (int y = 0; y < ySize; y++) {
+        cellHeight = Display.getHeight() / rowCount;
+        cellWidth = Display.getWidth() / colCount;
+        int status = 0;
+        for (int y = 0; y < rowCount; y++) {
             xLoc = 0;
-            for (int x = 0; x < xSize; x++) {
-                grid[x][y] = new GridCell(xLoc, yLoc, cellWidth, cellHeight);
-                xLoc += cellWidth;
+            for (int x = 0; x < colCount; x++) {
+                if (Math.random() < .3)
+                    status = 1;
+                else status = 0;
+                grid[x][y] = new GridCell(x, y, this, status);
             }
-            yLoc += cellHeight;
         }
+        int x = (int) (Math.random() * colCount);
+        int y = (int) (Math.random() * rowCount);
+        grid[x][y].setStatus(GridCell.PLAYER);
+        player = grid[x][y];
+
+        while (target == null) {
+            x = (int) (Math.random() * colCount);
+            y = (int) (Math.random() * rowCount);
+            if (grid[x][y].getStatus() == GridCell.EMPTY) {
+                grid[x][y].setStatus(GridCell.TARGET);
+                target = grid[x][y];
+            }
+        }
+
+        player.findTarget();
+    }
+
+    private void calculatePath() {
+
     }
 
     @Override
@@ -34,8 +59,8 @@ public class GridGame extends Scene {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 //        GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
 
-        for (int y = 0; y < ySize; y++) {
-            for (int x = 0; x < xSize; x++) {
+        for (int y = 0; y < rowCount; y++) {
+            for (int x = 0; x < colCount; x++) {
                 grid[x][y].draw();
             }
         }
